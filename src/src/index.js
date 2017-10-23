d7015454b2ec426f6a36632239a342ee09efaa53
Aidan Schonewille <schonea@mcmaster.ca>
2017-10-23 18:28:27 -0400

    Reformatted codebase for proper React usage

--- src/src/index.js
+++ src/src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import Video from './js/video';
-import TopBar from './js/topBar';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import Video from "./js/video";
+import TopBar from "./js/topBar";
 
 ReactDOM.render(
-	<TopBar/>,
-	document.getElementById('topBar')
-);
-
-ReactDOM.render(
-	<Video/>,
-	document.getElementById('video')
+  <div>
+    <TopBar />
+    <Video query="Gorillaz" />
+  </div>,
+  document.getElementById("root")
 );
--- src/src/js/search.js
+++ src/src/js/search.js
@@ -1,72 +1,46 @@
-//Array for videoIDs
-vid = [];
+import queryString from "query-string";
 
-export function searchVideo(PageToken)
-{
-    //Searchy text goes here
-    var searchText = "Radiohead";
-
-    //Scary JQuery get statement (note $ is shorthand for JQuery)
-    $.get(
-        //Youtube Data API request
-        "https://www.googleapis.com/youtube/v3/search",{
-        //Not certain what part means, but snippet works
-        part : 'snippet',
-        //Q for query
-        q : searchText,
-        //Max results, break over 50
-        maxResults : 10,
-        //No clue what a page token is
-        pageToken : PageToken,
-        //Lovely API key
-        key: 'AIzaSyAfwfRNJQRwEKJtea5hcZQ0hswGloi7DUI'},
-
-        //I also have no clue why this function is here
-        function myPlan(response){
-            //Again with the page tokens
-            nextPageToken=response.nextPageToken;     
-            //The number of results maybe?
-            var resultCount = response.pageInfo.totalResults;
-            //Or maybe thats what this is        
-            itemsLen = response.items.length;
-            //This loop populates our vid array
-            for (var i=0; i<response.items.length;i++){
-                var videoID =response.items[i].id.videoId;
-                //If the type is undefined we reject it (how do we know this? who knows)
-                if(typeof videoID != 'undefined'){
-                    vid.push(videoID);
-                    return;
-                }
-            }
-        searchVideo(nextPageToken);
-   }); //End of get nonsense
-}
-
-//Call Search Video
-searchVideo();
-
-
-//Setup Player
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-//Make the player
-var player;
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: vid[0],
-        events: {
-            'onReady': onPlayerReady,
+export default function searchVideo(query, pageToken, listOfVideoIDs = []) {
+  //Youtube Data API request
+  const params = {
+    //Not certain what part means, but snippet works
+    part: "snippet",
+    //Q for query
+    q: query,
+    //Max results, break over 50
+    maxResults: 10,
+    // Avoid playlists
+    type: 'video',
+    // Only search music videos
+    videoCategoryID: 10,
+    // To load the correct page of results
+    pageToken,
+    //Lovely API key
+    key: "AIzaSyAfwfRNJQRwEKJtea5hcZQ0hswGloi7DUI"
+  };
+  return fetch(
+    `https://www.googleapis.com/youtube/v3/search?${queryString.stringify(
+      params
+    )}`
+  )
+    .then(response => response.json())
+    .then(response => {
+      //Again with the page tokens
+      const nextPageToken = response.nextPageToken;
+      //This loop populates our vid array
+      for (let item of response.items) {
+        const videoID = item.id.videoId;
+        //If the type is undefined we reject it (how do we know this? who knows)
+        if (videoID !== undefined) {
+          listOfVideoIDs.push(videoID);
         }
+        // quit when we have 50 videos
+        if (listOfVideoIDs.length === 50) {
+          return listOfVideoIDs;
+        }
+      }
+      if (nextPageToken !== undefined) {
+        return searchVideo(query, nextPageToken, listOfVideoIDs);
+      }
     });
 }
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
-
